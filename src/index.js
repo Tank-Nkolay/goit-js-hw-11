c33bb6a76b82c37c9e0759e7c2b0c122bc01bf7b
Nikolay Tankov <tankov.n.k@gmail.com>
2022-08-28 19:13:57 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,83 +1,145 @@
-import './css/styles.css';
-import debounce from 'lodash.debounce';
+import PixabayApiService from './js/pixabay-API-service';
+// import imgTmpl from './js/ImgTpl';
+// import { renderImg } from './js/renderImg';
 import Notiflix from 'notiflix';
-import { fetchCountries } from './fetchCountries';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-const DEBOUNCE_DELAY = 300;
+// Уведомление
+// После первого запроса при каждом новом поиске выводить уведомление в котором будет написано сколько всего нашли изображений (свойство totalHits). Текст уведомления "Hooray! We found totalHits images."
 
-const input = document.querySelector('#search-box');
-const countryList = document.querySelector('.country-list');
-const countryInfo = document.querySelector('.country-info');
+// Notiflix.Notify.success('Hooray! We found totalHits images.');
 
-input.addEventListener('input', debounce(onFormInput, DEBOUNCE_DELAY));
+// В ответе бэкенд возвращает свойство totalHits - общее количество изображений которые подошли под критерий поиска (для бесплатного аккаунта). Если пользователь дошел до конца коллекции, пряч кнопку и выводи уведомление с текстом "We're sorry, but you've reached the end of search results."
 
-// функция - обработки вводимых данных по количеству символов ============================================
-function onFormInput(e) {
-  e.preventDefault();
-  countryList.innerHTML = '';
-  countryInfo.innerHTML = '';
+// Notiflix.Notify.info('We're sorry, but you've reached the end of search results.');
 
-  const inputData = input.value.trim();
-  if (inputData === '') {
-    return;
+const formEl = document.querySelector('#search-form');
+const input = document.querySelector('input');
+const loadMoreBtn = document.querySelector('.load-more');
+const imgContainer = document.querySelector('.gallery');
+
+const pixabayApiService = new PixabayApiService();
+
+formEl.addEventListener('submit', onSearch);
+
+loadMoreBtn.addEventListener('click', onLoadMore);
+
+function onSearch(event) {
+  event.preventDefault();
+
+  pixabayApiService.searchQuery =
+    event.currentTarget.elements.searchQuery.value.trim();
+  if (pixabayApiService.searchQuery === '') {
+    return Notiflix.Notify.warning('Write something');
   }
 
-  fetchCountries(inputData)
-    .then(country => {
-      if (country.length === 1) {
-        createCountryInfo(country);
-      } else if (country.length > 1 && country.length <= 10) {
-        createCountryList(country);
-      } else if (country.length > 10) {
-        onInputLotsOfData();
-      }
-    })
-    .catch(onError);
+  pixabayApiService.resetPage();
+
+  pixabayApiService.fetchImg().then(renderImg);
+  //  clearImgContainer();
+  //  renderImg(hits);
+  // .catch(onFetchError);
 }
 
-// функция - оповещение НЕТ СТРАН ДЛЯ ВЫВОДА ====================================================
-function onError() {
-  Notiflix.Notify.warning('Oops, there is no country with that name', {
-    position: 'center-top',
-  });
+// function renderImg(hits) {
+//   imgContainer.insertAdjacentHTML('beforeend', imgTmpl(hits));
+// }
+
+function renderImg(hits) {
+  const markupImg = hits
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => `<div class="photo-card">
+  <a class="gallery-item" href="${largeImageURL}"><img class="gallery-image" src="${webformatURL}" alt="${tags}" loading="lazy"/></a>
+  <div class="info">
+    <p class="info-item">
+      <b>Likes</b>${likes}
+    </p>
+    <p class="info-item">
+      <b>Views</b>${views}
+    </p>
+    <p class="info-item">
+      <b>Comments</b>${comments}
+    </p>
+    <p class="info-item">
+      <b>Downloads</b>${downloads}
+    </p>
+  </div>
+</div>`
+    )
+    .join();
+  imgContainer.insertAdjacentHTML('beforeend', markupImg);
 }
 
-// функция - оповещение СЛИШКОМ МНОГО СТРАН ДЛЯ ВЫВОДА ====================================================
-function onInputLotsOfData() {
-  Notiflix.Notify.info(
-    'Too many matches found. Please enter a more specific name.',
-    { position: 'center-top' }
-  );
+function onFetchError(error) {
+  Notiflix.Notify.warning('Oops, smth wrong');
 }
 
-// функция - добавить разметку список СТРАН ====================================================
-function createCountryList(e) {
-  const showCountry = e
-    .map(({ name, flags }) => {
-      return `<li class="country-list__item">
-        <img class="country-list__item--img" src='${flags.svg}' alt='Flag of ${name.official}' width="100">
-        <h2 class="country-list__item--header">${name.official}</h2>
-        </li>`;
-    })
-    .join('');
-  countryList.insertAdjacentHTML('beforeend', showCountry);
+function onLoadMore() {
+  pixabayApiService.fetchImg().then(renderImg);
+  // .then(renderImg);
 }
 
-// функция - добавить разметку поной информация СТРАНА ====================================================
-function createCountryInfo([{ name, flags, capital, population, languages }]) {
-  const showAllCountryInfo = ` 
-            <div class="country-info__cont">
-              <img class="country-info__item--img" src="${
-                flags.svg
-              }" alt="Flag of ${name.official}" width="100">
-              <h2 class="country-info__item--header">${name.official}</h2>
-            </div>
-            <ul class="country-info__list">
-                <li class="country-info__item"><span>Capital: </span>${capital}</li>
-                <li class="country-info__item"><span>Population: </span>${population}</li>
-                <li class="country-info__item"><span>Languages: </span>${Object.values(
-                  languages
-                ).join(', ')}</li>
-            </ul>`;
-  countryInfo.insertAdjacentHTML('beforeend', showAllCountryInfo);
+function clearImgContainer() {
+  imgContainer.innerHTML = '';
 }
+
+// За бажанням вищевказаний запит також можна виконати так
+// axios.get('/user', {
+//     params: {
+//       ID: 12345
+//     }
+//   })
+//   .then(function (response) {
+//     console.log(response);
+//   })
+//   .catch(function (error) {
+//     console.log(error);
+//   })
+//   .then(function () {
+//     // виконується завжди
+//   });
+
+// // Хочете використовувати async/await? Додайте ключове слово `async` до своєї зовнішньої функції/методу.
+// async function getUser() {
+//   try {
+//     const response = await axios.get('/user?ID=12345');
+//     console.log(response);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
+
+// const { height: cardHeight } = document
+//   .querySelector('.gallery')
+//   .firstElementChild.getBoundingClientRect();
+
+// window.scrollBy({
+//   top: cardHeight * 2,
+//   behavior: 'smooth',
+// });
+
+// Вместо кнопки «Load more» можно сделать бесконечную загрузку изображений при прокрутке страницы. Мы предоставлям тебе полную свободу действий в реализации, можешь использовать любые библиотеки.
+
+// Пагинация
+// Pixabay API поддерживает пагинацию и предоставляет параметры page и per_page. Сделай так, чтобы в каждом ответе приходило 40 объектов (по умолчанию 20).
+
+// Изначально значение параметра page должно быть 1.
+// При каждом последующем запросе, его необходимо увеличить на 1.
+// При поиске по новому ключевому слову значение page надо вернуть в исходное, так как будет пагинация по новой коллекции изображений.
+// В HTML документе уже есть разметка кнопки при клике по которой необходимо выполнять запрос за следующей группой изображений и добавлять разметку к уже существующим элементам галереи.
+
+// <button type="button" class="load-more">Load more</button>
+
+// Изначально кнопка должна быть скрыта.
+// После первого запроса кнопка появляется в интерфейсе под галереей.
+// При повторном сабмите формы кнопка сначала прячется, а после запроса опять отображается.
+// В ответе бэкенд возвращает свойство totalHits - общее количество изображений которые подошли под критерий поиска (для бесплатного аккаунта). Если пользователь дошел до конца коллекции, пряч кнопку и выводи уведомление с текстом "We're sorry, but you've reached the end of search results.".
---
+++ src/js/pixabay-API-service.js
@@ -0,0 +1,42 @@
+import axios from 'axios';
+
+const BASE_URL = 'https://pixabay.com/api/';
+const myKey = 'key=29439204-c21465a1feaf8a905890908f9';
+const elseParams =
+  'image_type=photo&orientation=horizontal&safesearch=true&per_page=40';
+// let items = [];
+
+export default class PixabayApiService {
+  constructor() {
+    this.query = '';
+    this.page = 1;
+  }
+
+  fetchImg() {
+    return fetch(
+      `${BASE_URL}?${myKey}&q=${this.query}&${elseParams}&page=${this.page}`
+    )
+      .then(response => response.json())
+      .then(data => {
+        this.incrementPage();
+
+        return data.hits;
+      });
+  }
+
+  incrementPage() {
+    this.page += 1;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+
+  get searchQuery() {
+    return this.query;
+  }
+
+  set searchQuery(newQuery) {
+    this.query = newQuery;
+  }
+}
